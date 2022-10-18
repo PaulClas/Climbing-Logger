ffc6b7cfc144f6a09818f591a5b4d59b21f159ec
Paul Clas <clas.paul@gmail.com>
2022-10-17 22:38:28 -0400

    Created bottom navigation bar

--- ClimbingTracker/App.js
+++ ClimbingTracker/App.js
@@ -1,23 +1,25 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import BottomNavBar from './app/components/bottomNavigationBar';
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.big}>B</Text>
+      {/* <Text style={styles.big}>B</Text>
       <Text>Beta</Text>
-      <Text>Climbing Log and Training App</Text>
+      <Text>Climbing Log and Training App</Text> */}
       <StatusBar style="auto" />
+      <BottomNavBar/>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   big: {
     fontSize: 100,
---
+++ ClimbingTracker/app/components/bottomNavigationBar.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import IonIcons from 'react-native-vector-icons/Ionicons';
+//Screens
+import HomeScreen from '../screens/HomeScreen';
+import ClimbingScreen from '../screens/ClimbingScreen';  
+import CommunityScreen from '../screens/CommunityScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+//Screen Names
+const homeName = "Home";
+const climbingName = "Climbing";
+const communityName = "Community";
+const profileName = "Profile";
+
+
+
+const tab= createBottomTabNavigator();
+
+export default function BottomNavBar(){
+    return(
+        <NavigationContainer>
+            <tab.Navigator
+                initialRouteName={homeName}
+                screenOptions={({route}) => ({
+                    tabBarActiveTintColor: '#e91e63',
+                    tabBarInactiveTintColor: 'black',
+                    tabBarLabelStyle: {fontSize: 10, paddingBottom: 10},
+                    tabBarStyle: {height: 70, padding:10},
+                    tabBarIcon: ({focused, color, size}) => {
+                        let iconName;
+                        let rn = route.name;
+                        if(rn === homeName){
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if(rn === climbingName){
+                            iconName = focused ? 'fitness' : 'fitness-outline';
+                        } else if(rn === communityName){
+                            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+                        } else if(rn === profileName){
+                            iconName = focused ? 'person' : 'person-outline';
+                        }
+                        return <IonIcons name={iconName} size={size} color={color} />
+                    },
+                })}>
+                <tab.Screen name={homeName} component={HomeScreen} />
+                <tab.Screen name={climbingName} component={ClimbingScreen} />
+                <tab.Screen name={communityName} component={CommunityScreen} />
+                <tab.Screen name={profileName} component={ProfileScreen} />
+            </tab.Navigator>
+        </NavigationContainer>
+
+    )
+}
