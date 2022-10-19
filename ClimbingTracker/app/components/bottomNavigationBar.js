327aa92dbb68576bc99406d92aa0ea3cbfde421a
Paul Clas <clas.paul@gmail.com>
2022-10-19 15:07:46 -0400

    Bottom General nav done

--- ClimbingTracker/app/components/bottomNavigationBar.js
+++ ClimbingTracker/app/components/bottomNavigationBar.js
@@ -10,85 +10,52 @@ import ClimbingScreen from '../screens/ClimbingScreen';
 import CommunityScreen from '../screens/CommunityScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import GymScreen from '../screens/MygymScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 
 //Screen Names
 const homeName = "Home";
 const climbingName = "Climbing";
-const communityName = "Community";
+const communityName = "Chat";
 const profileName = "Profile";
-const gymName = "My Gym";
-const settingsName= "Settings";
-
-function HeaderOptions() {
-    const navigation = useNavigation();
-    return (
-            <View style={{
-                flexDirection: 'row',
-            }}>
-                <IonIcons 
-                    name="wallet-outline" 
-                    size={30} 
-                    style={{padding:10}}
-                    onPress={() => navigation.navigate(gymName)}/>
-                <IonIcons 
-                    name="settings-outline" 
-                    size={30} 
-                    style={{padding:10}}
-                    onPress={() => navigation.navigate(settingsName)}/>
-            </View>
-    );
-}
-
-function BottomNav(){
-    return(
-        <tab.Navigator
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    let rn = route.name;
-                    if(rn === homeName){
-                        iconName = focused ? 'home' : 'home-outline';
-                    } else if(rn === climbingName){
-                        iconName = focused ? 'barbell' : 'barbell-outline';
-                    } else if(rn === communityName){
-                        iconName = focused ? 'people' : 'people-outline';
-                    } else if(rn === profileName){
-                        iconName = focused ? 'person' : 'person-outline';
-                    }
-                    return <IonIcons name={iconName} size={size} color={color} />
-                },
-            })}
-            tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-            }}
-        >
-            <tab.Screen 
-                name={homeName} 
-                component={HomeScreen}
-                options={{ 
-                    headerRight:() =>(
-                        <HeaderOptions/>
-                    )
-                }} />
-            <tab.Screen name={climbingName} component={ClimbingScreen} />
-            <tab.Screen name={communityName} component={CommunityScreen} />
-            <tab.Screen name={profileName} component={ProfileScreen} />
-        </tab.Navigator>
-    )
-}
+const gymName = "Search";
 
 const tab= createBottomTabNavigator();
-
-const stack = createNativeStackNavigator();
 
 export default function BottomNavBar(){
     return(
         <NavigationContainer>
-            <BottomNav/>
-            <stack.Screen name={gymName} component={GymScreen} />
-            <stack.Screen name={settingsName} component={SettingsScreen} />
+            <tab.Navigator
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                        let iconName;
+                        let rn = route.name;
+                        if(rn === homeName){
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if(rn === gymName){
+                            iconName = focused ? 'search' : 'search-outline';
+                        } else if(rn === climbingName){
+                            iconName = focused ? 'play-circle' : 'play-circle-outline';
+                            size=50;
+                        } else if(rn === communityName){
+                            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+                        } else if(rn === profileName){
+                            iconName = focused ? 'person' : 'person-outline';
+                        }
+                        return <IonIcons name={iconName} size={size} color={color} />
+                    },
+                    tabBarStyle: {height:80, backgroundColor: '#fff', borderTopWidth: 0, elevation: 0},
+                    tabBarLabelStyle: {fontSize: 12, fontWeight: 'bold', marginBottom: 10},
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray',
+                }}
+            >
+                <tab.Screen name={homeName} component={HomeScreen} />
+                <tab.Screen name={gymName} component={GymScreen} />
+                <tab.Screen name={climbingName} component={ClimbingScreen} />
+                <tab.Screen name={communityName} component={CommunityScreen} />
+                <tab.Screen name={profileName} component={ProfileScreen} />
+            </tab.Navigator>
         </NavigationContainer>
     
 
